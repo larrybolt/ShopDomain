cb1cb9c0cb5b16d4dd207f59ce65397315f9891d
annelore van buekenhout <xnlore@hotmail.com>
2015-12-08 11:31:04 +0100

    ShopFacade vervolledig(d|t) :D

--- src/domain/ShopFacade.java
+++ src/domain/ShopFacade.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ShopFacade {
 	private ProductService productService;
@@ -53,6 +54,50 @@ public class ShopFacade {
 	public void removeObserver(Observer observer){
 		verkoop.removeObserver(observer);
 	}
+	//PersonService
+	public Person getPerson(int id){
+		return personService.getPerson(id);
+	}
+	public void addPerson(Person p){
+		personService.addPerson(p);
+	}
+	public void deletePerson(int id){
+		personService.deletePerson(id);
+	}
+	public void deletePerson(String id){
+		personService.deletePerson(id);
+	}
+	public Person getPersonByEmail(String email){
+		return personService.getPersonByEmail(email);
+	}
+	public Person authenticate(String email, String password){
+		return personService.authenticate(email, password);
+	}
+	public List<Person> getPersons(){
+		return personService.getPersons();
+	}
+	//ProductService
 	
-	
+	public void addProductinDB(Product product){
+		productService.addProduct(product);
+	}
+	public void deleteProductinDB(String id){
+		productService.deleteProduct(id);
+	}
+	public Product getProduct(int id){
+		return productService.getProduct(id);
+	}
+	public Product getProduct(String id){
+		return productService.getProduct(id);
+	}
+	public List<Product> getProductsFromDB(){
+		return productService.getProducts();
+	}
+	public List<Product> getProductsFromDBOrderByPrice(){
+		return productService.getProductsOrderByPrice();
+	}
+
+	public void updateProducts(Product p) {
+		 productService.updateProducts(p);
+	}
 }
